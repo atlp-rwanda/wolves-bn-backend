db1b58c93adecfa22e973ba7cedd8ec099251e24
Cherubin <cherubinkubwayo@gmail.com>
2020-09-14 19:09:42 +0200

    chore(database): configure postgresql

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import {createUser} from '../controllers'
 const router = express.Router();
 router.use(express.json());
 // Test if endpoint works
 router.get("/", (req, res) => {
   res.send("Get endpoint is working");
 });
-
+router.post('/user', createUser)
 export default router;
