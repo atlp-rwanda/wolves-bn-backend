fbdbbcd1bc4ac390a0fb0495764211fed7dd6f7e
IreneRukumbuzi <rukkirene21@gmail.com>
2020-09-15 17:52:51 +0200

    Fixing parantheses in index controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ import { User } from '../database/models/user';
 const createUser = (req, res) => User.create({
   name: req.body.name,
 })
-  .then((user) => res.status(201).send(user))
-  .catch((error) => res.status(400).send(error));
+  .then((user) => { res.status(201).send(user); })
+  .catch((error) => { res.status(400).send(error); });
 
 export default createUser;
