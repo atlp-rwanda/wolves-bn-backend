965b29b9b540efa18da2c0269e1a114abdad8da6
Cherubin <cherubinkubwayo@gmail.com>
2020-09-16 15:07:23 +0200

    chore(database): add test environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 module.exports =  {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
     dialect: "postgres"
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
     dialect: "postgres"
   },
   production: {
