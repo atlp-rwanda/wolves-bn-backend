32a866ceadc60be7120a967a78f13eb3b6163244
Cherubin <cherubinkubwayo@gmail.com>
2020-09-16 06:58:22 +0200

    chore(api-docs):setup swagger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import router from './routes/index';
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', router);
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log(`Server has started at port ${PORT}`);
 });
