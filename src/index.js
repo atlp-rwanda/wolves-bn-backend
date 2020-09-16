03b96d17e209ff94b9baec5127bd7646c885ea2a
Cherubin <cherubinkubwayo@gmail.com>
2020-09-16 07:28:54 +0200

    chore(api-docs): fix single quotes issue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
+
 import 'dotenv/config';
+
+import swaggerUi from 'swagger-ui-express';
+
 import router from './routes/index';
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
+
+import swaggerDocument from '../swagger.json';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', router);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
-  console.log(`Server has started at port ${PORT}`);
+  console.log('Server has started at port', PORT);
 });
 
 export default app;
