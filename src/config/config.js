f2515b8c2017a89e7a0d387cdfedb7bc359e7e61
anisie <auwimana@aimsammi.org>
2020-09-17 16:32:56 +0200

    Update Sequelize configurations to use environment variable in production [Heroku]

--- src/config/config.js
+++ src/config/config.js
@@ -2,22 +2,18 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV_NAME,
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL,
   },
 };
