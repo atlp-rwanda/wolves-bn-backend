54b8e27d8991fb992d7dacb226d964a34d1396b2
IreneRukumbuzi <rukkirene21@gmail.com>
2020-10-21 11:00:13 +0200

    Handling errors

--- src/services/trip.notification.js
+++ src/services/trip.notification.js
@@ -26,87 +26,91 @@ export default class TripNotification {
   }
 
   static async sendTripNotification() {
-    await emitter.on('request-created', async (tripRequest) => {
-      const {
-        managerEmail,
-        managerNames
-      } = await this.findManagerToNotify(tripRequest);
+    try {
+      await emitter.on('request-created', async (tripRequest) => {
+        const {
+          managerEmail,
+          managerNames
+        } = await this.findManagerToNotify(tripRequest);
 
-      const {
-        firstName, lastName
-      } = await users.findOne({ where: { id: tripRequest.requester_id } });
-      const message = `New ${tripRequest.travel_type} travelling on ${tripRequest.travel_date} have been requested by ${firstName} ${lastName}`;
+        const {
+          firstName, lastName
+        } = await users.findOne({ where: { id: tripRequest.requester_id } });
+        const message = `New ${tripRequest.travel_type} travelling on ${tripRequest.travel_date} have been requested by ${firstName} ${lastName}`;
 
-      const notificationData = await NotificationService.createNotification({
-        message,
-        notificationOwner: tripRequest.manager_id,
-        tripId: tripRequest.id
+        const notificationData = await NotificationService.createNotification({
+          message,
+          notificationOwner: tripRequest.manager_id,
+          tripId: tripRequest.id
+        });
+        const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
+        const actionLink = `http://${BASEURL}:${PORT}/api/trips/${tripRequest.id}`;
+        const msg = emailNotification(managerNames, message, actionLink, unsubscribeUrl);
+        sock.broadcast.emit('new-notification', notificationData);
+        SendingMail.sendGridMail(managerEmail, msg);
       });
-      const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
-      const actionLink = `http://${BASEURL}:${PORT}/api/trips/${tripRequest.id}`;
-      const msg = emailNotification(managerNames, message, actionLink, unsubscribeUrl);
-      sock.broadcast.emit('new-notification', notificationData);
-      SendingMail.sendGridMail(managerEmail, msg);
-    });
-    await emitter.on('request-updated', async (data) => {
-      const {
-        managerEmail,
-        managerNames
-      } = await this.findManagerToNotify(data);
+      await emitter.on('request-updated', async (data) => {
+        const {
+          managerEmail,
+          managerNames
+        } = await this.findManagerToNotify(data);
 
-      const {
-        firstName, lastName
-      } = await users.findOne({ where: { id: data.requester_id } });
-      const message = `${data.travel_type} by ${firstName} ${lastName} has been updated to travel on ${data.travel_date}`;
+        const {
+          firstName, lastName
+        } = await users.findOne({ where: { id: data.requester_id } });
+        const message = `${data.travel_type} by ${firstName} ${lastName} has been updated to travel on ${data.travel_date}`;
 
-      const notificationData = await NotificationService.createNotification({
-        message,
-        notificationOwner: data.manager_id,
-        tripId: data.id
+        const notificationData = await NotificationService.createNotification({
+          message,
+          notificationOwner: data.manager_id,
+          tripId: data.id
+        });
+        const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
+        const actionLink = `http://${BASEURL}:${PORT}/api/trips/${data.id}`;
+        const msg = emailNotification(managerNames, message, actionLink, unsubscribeUrl);
+        sock.broadcast.emit('new-notification', notificationData);
+        SendingMail.sendGridMail(managerEmail, msg);
       });
-      const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
-      const actionLink = `http://${BASEURL}:${PORT}/api/trips/${data.id}`;
-      const msg = emailNotification(managerNames, message, actionLink, unsubscribeUrl);
-      sock.broadcast.emit('new-notification', notificationData);
-      SendingMail.sendGridMail(managerEmail, msg);
-    });
-    await emitter.on('request-status-updated', async (data) => {
-      const {
-        firstName, lastName, email
-      } = await users.findOne({ where: { id: data.requester_id } });
-      const userNames = `${firstName} ${lastName}`;
-      const userEmail = `${email}`;
-      const message = `Your trip request has been ${data.request_status} by your manager`;
+      await emitter.on('request-status-updated', async (data) => {
+        const {
+          firstName, lastName, email
+        } = await users.findOne({ where: { id: data.requester_id } });
+        const userNames = `${firstName} ${lastName}`;
+        const userEmail = `${email}`;
+        const message = `Your trip request has been ${data.request_status} by your manager`;
 
-      const notificationData = await NotificationService.createNotification({
-        message,
-        notificationOwner: data.requester_id,
-        tripId: data.id
+        const notificationData = await NotificationService.createNotification({
+          message,
+          notificationOwner: data.requester_id,
+          tripId: data.id
+        });
+        const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
+        const msg = requestEmail(userNames, message, unsubscribeUrl);
+        sock.broadcast.emit('new-notification', notificationData);
+        SendingMail.sendGridMail(userEmail, msg);
       });
-      const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
-      const msg = requestEmail(userNames, message, unsubscribeUrl);
-      sock.broadcast.emit('new-notification', notificationData);
-      SendingMail.sendGridMail(userEmail, msg);
-    });
-    await emitter.on('comment-created', async (data) => {
-      const { firstName, lastName, email } = await users.findOne({ where: { id: data.userId } });
-      const { travel_type, from, to } = await trip.findOne({ where: { id: data.tripId } });
+      await emitter.on('comment-created', async (data) => {
+        const { firstName, lastName, email } = await users.findOne({ where: { id: data.userId } });
+        const { travel_type, from, to } = await trip.findOne({ where: { id: data.tripId } });
 
-      const userNames = `${firstName} ${lastName}`;
-      const userEmail = `${email}`;
-      const message = `${userNames} has commented on ${travel_type} from ${from} to ${to}`;
+        const userNames = `${firstName} ${lastName}`;
+        const userEmail = `${email}`;
+        const message = `${userNames} has commented on ${travel_type} from ${from} to ${to}`;
 
-      const notificationData = await NotificationService.createNotification({
-        message,
-        notificationOwner: data.userId,
-        tripId: data.tripId
+        const notificationData = await NotificationService.createNotification({
+          message,
+          notificationOwner: data.userId,
+          tripId: data.tripId
+        });
+
+        const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
+        const actionLink = `http://${BASEURL}:${PORT}/api/trips/${data.tripId}`;
+        const msg = emailNotification(userNames, message, actionLink, unsubscribeUrl);
+        sock.broadcast.emit('new-notification', notificationData);
+        SendingMail.sendGridMail(userEmail, msg);
       });
-
-      const unsubscribeUrl = `http://${BASEURL}:${PORT}/api/notifications`;
-      const actionLink = `http://${BASEURL}:${PORT}/api/trips/${data.tripId}`;
-      const msg = emailNotification(userNames, message, actionLink, unsubscribeUrl);
-      sock.emit('new-notification', notificationData);
-      SendingMail.sendGridMail(userEmail, msg);
-    });
+    } catch (error) {
+      return error.message;
+    }
   }
 }
